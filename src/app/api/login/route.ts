ba331005e77e2c1c415760d7280ed9daa1082b27
Miloua Mokhtar <80211751+Miloua91@users.noreply.github.com>
2024-03-22 18:24:28 +0100

    user logins with otp

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,38 +2,50 @@ import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../../../../database.types";
 
+const twofactor = require("node-2fa");
+
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require("twilio")(accountSid, authToken);
-
 export async function GET(req: NextRequest) {
   const user = req.cookies.get("bookworm");
+  const token = req.cookies.get("otp");
+  console.log(token?.value);
   if (!user)
     return NextResponse.json({
       error: "No user sent from the client.",
     });
+  if (!token)
+    return NextResponse.json({
+      error: "No token sent from the client.",
+    });
+
   try {
     let { data: bookworm, error } = await supabase
       .from("bookworm")
-      .select("id, username")
+      .select("id, username, secret")
       .eq("username", `${user.value}`);
 
-    const newFactorResponse = await client.verify.v2
-      .services("VA2569c344f0ec746da93abb75412c1a76")
-      .entities(bookworm[0].id)
-      .newFactors.create({
-        friendlyName: `Library Tower`,
-        factorType: "totp",
+    const isValidToken = twofactor.verifyToken(bookworm[0].secret, token.value);
+    if (isValidToken.delta === 0)
+      return NextResponse.json({
+        data: bookworm,
+        error,
       });
 
-    const link = newFactorResponse;
+    if (isValidToken.delta !== 0)
+      return NextResponse.json({
+        expired: "Expired 2FA token.",
+      });
 
-    return NextResponse.json({ data: bookworm, error, link });
+    if (isValidToken === null)
+      return NextResponse.json({
+        error: "Invalid 2FA token.",
+      });
+
+    return NextResponse.json({ data: bookworm, error });
   } catch (err) {
     return NextResponse.json({ err });
   }
